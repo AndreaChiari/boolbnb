6846dd993b88c0119c6e753573a7c378b5580643
Alessandro-Villani <alessvill@libero.it>
2023-03-29 18:28:10 +0200

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    name: "BoolBnB",
+    components: { AppHeader }
+}
 
 </script>
 
 <template>
+    <AppHeader />
     <RouterView />
 </template>
 
-<style></style>
+<style lang="scss">
+@use './assets/styles/style.scss';
+</style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,45 @@
 <script>
+import { RouterLink } from 'vue-router';
 
+export default {
+    name: "AppHeader",
+    components: { RouterLink }
+}
 </script>
 
-<template></template>
+<template>
+    <header>
+        <nav class="container d-flex justify-content-between align-items-center py-3 h-100">
+            <div class="nav-left h-100">
+                <router-link :to="{ name: 'home' }"><img
+                        src="https://loghi-famosi.com/wp-content/uploads/2020/07/Airbnb-Logo.png" alt="logo"></router-link>
+            </div>
+            <div class="nav-center flex-grow-1 px-5">
+                <div class="input-group ">
+                    <input type="text" class="form-control" placeholder="Recipient's username"
+                        aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-secondary" type="button" id="button-addon2"><i
+                            class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+            </div>
+            <div class="nav-right">
+                <router-link :to="{ name: 'index' }" class="btn btn-primary me-2">Login</router-link>
+                <router-link to="/apartments/1" class="btn btn-secondary">Sign In</router-link>
+            </div>
+        </nav>
+    </header>
+</template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+
+header {
+    height: 80px;
+    background-color: $secondary-bg;
+    color: white;
+
+    .nav-left img {
+        max-height: 100%;
+    }
+}
+</style>
