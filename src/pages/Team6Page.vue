ac12bb85d5053a206c73115ad9977b48f6535ed1
AndreaChiari <chiariandrea94@gmail.com>
2023-04-07 12:24:17 +0200

    update-team6page

--- src/pages/Team6Page.vue
+++ src/pages/Team6Page.vue
@@ -3,6 +3,7 @@
         <div class="container">
             <div class="cards d-flex justify-content-center flex-wrap py-5">
                 <div class="card rounded-5" style="width: 18rem;">
+                    <a href="https://www.linkedin.com/in/alessandro-villani/"></a>
                     <img src="public/img/ale.jpeg" alt="" class="rounded-5">
                     <div class="card-body">
                         <h5 class=" text-center">Alessandro <span>Villani</span></h5>
@@ -10,6 +11,7 @@
                     </div>
                 </div>
                 <div class="card rounded-5" style="width: 18rem;">
+                    <a href="https://www.linkedin.com/in/andrea-chiari-55b9bb24a/"></a>
                     <img src="public/img/andrea.jpeg" alt="" class="rounded-5">
                     <div class="card-body">
                         <h5 class=" text-center">Andrea <span>Chiari</span></h5>
@@ -18,13 +20,15 @@
                 </div>
                 <div class="card rounded-5" style="width: 18rem;">
                     <img src="public/img/ant.jpeg" alt="" class="rounded-5">
+                    <a href="https://www.linkedin.com/in/antonio-novelli-dev/"></a>
                     <div class="card-body">
                         <h5 class=" text-center">Antonio <span>Novelli</span></h5>
                         <p class="card-text"></p>
                     </div>
                 </div>
                 <div class="card rounded-5" style="width: 18rem;">
-                    <img src="public/img/rob.jpeg" alt="" class="rounded-5">
+                    <a href="https://www.linkedin.com/in/roberto-simonini-370733117/"></a>
+                    <img src="public/img/rob.jpg" alt="" class="rounded-5">
                     <div class="card-body">
                         <h5 class=" text-center">Roberto <span>Simonini</span></h5>
                         <p class="card-text"></p>
@@ -49,6 +53,15 @@ main {
         filter: grayscale(1);
         transform: scale(1);
         transition: transform 0.3s ease-in-out;
+        position: relative;
+
+        a {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+        }
 
         &:hover {
             transform: scale(1.1);
