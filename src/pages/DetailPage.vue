1aadee8bc92480e8e7ba42b3a76612aa2266a1d0
antonionovelli92 <anto.novelli@hotmail.it>
2023-04-17 12:06:21 +0200

    fix layout

--- src/pages/DetailPage.vue
+++ src/pages/DetailPage.vue
@@ -100,60 +100,75 @@ export default {
     <main v-if="!isLoading" class="py-5">
         <div v-if="apartment" class="container apartment">
             <AppAlert v-if="showAlert" :type="alertType" :text="alertText" @close-alert="showAlert = false" />
-            <div class="pictures row mx-0 px-0 mb-3">
-                <div class="pic-holder border-pink p-0" :class="apartment.apartment_pics.length ? 'col-6' : 'col-12'">
-                    <img class="main-pic" :src="apartment.thumb" :alt="apartment.name">
-                </div>
-                <div v-if="apartment.apartment_pics.length" class="pic-holder  col-6 p-0 mx-0 row side-pics">
-                    <div class="border-pink p-0" :class="picHolderClassSelection(i)"
-                        v-for="pic, i in apartment.apartment_pics">
-                        <img class="secondary-pic" :src="`http://127.0.0.1:8000/storage/${pic.thumb}`" :alt="pic.id">
+
+            <div class="row">
+                <div class="col-md-12 col-lg-6 mb-3 mb-lg-0">
+                    <div class="col-12 py-2 mb-3 border-bottom">
+                        <h1 class="apartment-name">{{ apartment.name }}</h1>
+                    </div>
+
+                    <div class="pictures row mx-0 px-0 mb-3">
+                        <div class="pic-holder border-pink p-0"
+                            :class="apartment.apartment_pics.length ? 'col-6' : 'col-12'">
+                            <img class="main-pic" :src="apartment.thumb" :alt="apartment.name">
+                        </div>
+                        <div v-if="apartment.apartment_pics.length" class="pic-holder col-6 p-0 mx-0 row side-pics">
+                            <div class="border-pink p-0" :class="picHolderClassSelection(i)"
+                                v-for="pic, i in apartment.apartment_pics">
+                                <img class="secondary-pic" :src="`http://127.0.0.1:8000/storage/${pic.thumb}`"
+                                    :alt="pic.id">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="info row border-bottom align-items-center">
+                        <div class="col-6 pt-3">
+                            <address class="mb-0">{{ apartment.address }}</address>
+                        </div>
+                        <div class="col-6 pt-3 text-end">
+                            <h4 class="apartment-price">€{{ apartment.price }}/notte</h4>
+                        </div>
+                    </div>
+                    <div class="details row">
+                        <div class="col-12 py-5 border-bottom">
+                            <p class="description mb-0">{{ apartment.description }}</p>
+                        </div>
+                        <div class="col-md-4 py-5 text-center border border-top-none border-left-none">
+                            <i class="fa-solid fa-person-shelter fa-2x me-2"></i><span class="h2">{{ apartment.rooms
+                            }}</span>
+                        </div>
+                        <div class="col-md-4 py-5 text-center border border-top-none">
+                            <i class="fa-solid fa-bed fa-2x me-2"></i><span class="h2">{{ apartment.beds }}</span>
+                        </div>
+                        <div class="col-md-4 py-5 text-center border border-top-none border-right-none">
+                            <i class="fa-solid fa-restroom fa-2x me-2"></i><span class="h2">{{ apartment.bathrooms }}</span>
+                        </div>
+                    </div>
+                    <div v-if="apartment.services.length" class="services row">
+                        <div class="col-12 py-3">
+                            <h3>Servizi</h3>
+                        </div>
+                        <ul class="col-12 mb-0">
+                            <li v-for="service in apartment.services" class="border-bottom py-2">
+                                <i class="me-2" :class="service.icon"></i>{{ service.name }}
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="buttons row mt-3">
+                        <div class="col-12 d-flex justify-content-between">
+                            <button class="contact-button" @click="$router.go(-1)">
+                                Torna Indietro
+                            </button>
+                            <GeneralButton buttonText="Contatta il proprietario" @button-click="contactModal = true" />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="info row border-bottom align-items-center">
-                <div class="col-6 pt-3">
-                    <address class="mb-0">{{ apartment.address }}</address>
-                </div>
-                <div class="col-6 pt-3">
-                    <h4 class="apartment-price text-end">€{{ apartment.price }}/notte</h4>
+                <div class="col-lg-4 p-5">
+                    <app-map :flag="false" :name="apartment.name" :coordinates="coordinates"></app-map>
                 </div>
             </div>
-            <div class="details row">
-                <div class="col-12 py-5 border-bottom">
-                    <p class="description mb-0">{{ apartment.description }}</p>
-                </div>
-                <div class="col-4 py-5 text-center border border-top-none border-left-none"><i
-                        class="fa-solid fa-person-shelter fa-2x me-2"></i><span class="h2">{{
-                            apartment.rooms }}</span>
-                </div>
-                <div class="col-4 py-5 text-center border border-top-none"><i class="fa-solid fa-bed fa-2x me-2"></i><span
-                        class="h2">{{
-                            apartment.beds }}</span></div>
-                <div class="col-4 py-5 text-center border border-top-none border-right-none"><i
-                        class="fa-solid fa-restroom fa-2x me-2"></i><span class="h2">{{
-                            apartment.bathrooms }}</span>
-                </div>
-            </div>
-            <div v-if="apartment.services.length" class="services row">
-                <div class="col-12 py-3">
-                    <h3>Servizi</h3>
-                </div>
-                <ul class="col-12 mb-0">
-                    <li v-for="service in apartment.services" class="border-bottom py-2"><i class="me-2"
-                            :class="service.icon"></i>{{
-                                service.name }}</li>
-                </ul>
-            </div>
-            <div class="buttons row mt-3">
-                <div class="col-12 d-flex justify-content-between">
-                    <button class="contact-button" @click="$router.go(-1)">Torna Indietro</button>
-                    <GeneralButton buttonText="Contatta il proprietario" @button-click="contactModal = true" />
-                </div>
-            </div>
+
             <ContactModal v-if="contactModal" :contact="apartment.name" :id="apartment.id"
                 @close-modal="contactModal = false" @send-form="sendMessage" />
-            <app-map :name="apartment.name" :coordinates="coordinates"></app-map>
         </div>
     </main>
 </template>
@@ -236,6 +251,17 @@ main {
         height: 100%;
         width: 100%;
         object-fit: cover;
+        transition: all 0.3s ease-out;
+
+
+
+        &:active {
+            border: 1px solid #ff385c;
+            transform: scale(1.5);
+            position: relative;
+            z-index: 3;
+            cursor: pointer;
+        }
     }
 
     .info {
@@ -280,5 +306,31 @@ main {
         color: $pink-3;
     }
 
+}
+
+#map {
+    height: 400px;
+    width: 500px;
+    color: rgb(211, 56, 56);
+    border-radius: 20px;
+    border: 3px solid white;
+    box-shadow: 0 0 30px black;
+    padding: 20px;
+    margin-right: 10px;
+
+    &:hover {
+        border: 3px solid rgb(255, 90, 95);
+    }
+
+}
+
+@media screen and (max-width: 767px) {
+    #map {
+        height: 300px;
+        width: 700px;
+        width: 90%;
+        margin: 0 auto;
+        padding: 0;
+    }
 }
 </style>
